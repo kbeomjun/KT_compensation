32420ac423ce6c5fa6cc319cfa7e62b26e19504b
beomjun kim <smatgon@naver.com>
2025-06-10 08:03:53 +0000

    outofstock, updatestatus

--- order/src/main/java/compensation/domain/Order.java
+++ order/src/main/java/compensation/domain/Order.java
@@ -47,18 +47,14 @@ private String address;
 
     @PostPersist
     public void onPostPersist(){
-    Inventory inventory = OrderApplication.applicationContext
-        .getBean(compensation.external.InventoryService.class)
-        .checkStock(get??);
-
 
         OrderPlaced orderPlaced = new OrderPlaced(this);
         orderPlaced.publishAfterCommit();
 
 
 
-        OrderCancelled orderCancelled = new OrderCancelled(this);
-        orderCancelled.publishAfterCommit();
+        // OrderCancelled orderCancelled = new OrderCancelled(this);
+        // orderCancelled.publishAfterCommit();
 
     
     }
@@ -82,17 +78,12 @@ private String address;
 
         */
 
-        /** Example 2:  finding and process
-        
-
-        repository().findById(outOfStock.get???()).ifPresent(order->{
+        repository().findById(outOfStock.getOrderId()).ifPresent(order->{
             
-            order // do something
+            order.setStatus("OrderCancelled");
             repository().save(order);
 
-
          });
-        */
 
         
     }
